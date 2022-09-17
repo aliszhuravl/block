51d8a0b5080a1e27d5ef7f1df345f8ac32749125
aliszhuravl <alis.zhuravl@gmail.com>
2022-09-17 18:42:24 +0300

    new

--- web/js/main.js
+++ web/js/main.js
@@ -61,6 +61,7 @@ window.onload = function() {
 })(jQuery);
 $(document).ready(function() {
     $(".services_container__inner_box").hover(function () {
+        $('.active_box').removeClass('active_box');
         $(this).addClass('active_box');
         $(this).siblings().removeClass('active_box');
     });
